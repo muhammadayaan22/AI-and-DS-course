5da0b05feec5dc1ad7b80218b4ed2f82f4861202
muhammadayaan22 <muhammadayaan2217@gmail.com>
2025-08-28 05:15:17 -0700

    student record management system

---
+++ srms.py
@@ -0,0 +1,33 @@
+#student record management system
+
+print("------STUDENT RECORDS-------")
+
+records = []
+
+while True:
+    print("---------------------")
+    print("1. Add Student")
+    print("2. Display Student")
+    print("3. Add Student")
+    print("4. Add Student")
+    print("5. Add Student")
+    print("6. Add Student")
+    print("7. Exit")
+    print("----------------------")
+
+    choice = input("Enter your choice: ")
+
+    if choice == '1':
+        pass
+    elif choice == '2':
+        pass
+    elif choice == '3':
+        pass
+    elif choice == '4':
+        pass
+    elif choice == '5':
+        pass
+    elif choice == '6':
+        pass
+    elif choice == '7':
+        break
